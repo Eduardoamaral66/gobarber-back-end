2922e9714eb958250e0391fd95afcfddbcae3f2f
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-05-12 19:13:25 -0300

    feat: finish routers and controllers

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import ProvidersController from '../controllers/ProvidersController';
+import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProvidersMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const availabilityMonthController = new ProvidersMonthAvailabilityController();
+const availabilityDayController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  availabilityDayController.index,
+);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  availabilityMonthController.index,
+);
 
 export default providersRouter;
