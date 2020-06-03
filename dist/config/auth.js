9419358fc88f917913f3ccecbc703349f14d07dc
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-06-02 23:15:28 -0300

    refactor: remove dist folder

--- dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: '12e61796b757425eef401c7753b56e53',
-        expiresIn: '1d',
-    },
-};
