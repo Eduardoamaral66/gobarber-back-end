26e96ecac7da4f23b10052bc6263c2612821d87d
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-06-22 18:55:59 -0300

    fix: fix classToClass providers transform

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,16 +2,17 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+import { classToClass } from 'class-transformer';
 
 export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const listProvidersService = container.resolve(ListProvidersService);
 
-    const appointment = await listProvidersService.execute({
+    const providers = await listProvidersService.execute({
       user_id,
     });
 
-    return response.json(appointment);
+    return response.json(classToClass(providers));
   }
 }
--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepositories from '@modules/users/repositories/IUsersRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -28,7 +29,10 @@ export default class ListProvidersService {
       });
     }
 
-    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    await this.cacheProvider.save(
+      `providers-list:${user_id}`,
+      classToClass(users),
+    );
     return users;
   }
 }
